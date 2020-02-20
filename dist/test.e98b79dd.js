67e2d2f2e24a514eb4c6af4f35392349aa7689e6
Antonin Langlade <antonin.langlade@betc.com>
2020-02-20 17:51:15 +0100

    Favicon

--- dist/test.e98b79dd.js
+++ dist/test.e98b79dd.js
@@ -1501,7 +1501,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60981" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51946" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
